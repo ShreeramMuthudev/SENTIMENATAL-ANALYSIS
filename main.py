3e0503a91e5e5443bd0faf8cfd0d828a96bbfff1
Shreeram Muthudev <109138114+ShreeramMuthudev@users.noreply.github.com>
2023-12-15 02:46:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,58 @@
 import streamlit as st
 import pickle
+import nltk# Path to your NLTK data directory (AppData location)
+import os
+nltk_data_dir = os.path.expanduser('~\\AppData\\Roaming\\nltk_data')
 
+# Add the NLTK data directory to NLTK's data path
+nltk.data.path.append(nltk_data_dir)
+
+# Check if stopwords and punkt are present in the nltk_data_dir
+stopwords_present = os.path.exists(os.path.join(nltk_data_dir, 'corpora/stopwords'))
+punkt_present = os.path.exists(os.path.join(nltk_data_dir, 'tokenizers/punkt'))
+
+if not stopwords_present:
+    # If stopwords not present, download them
+    nltk.download('stopwords', download_dir=nltk_data_dir)
+
+if not punkt_present:
+    # If punkt not present, download it
+    nltk.download('punkt', download_dir=nltk_data_dir)
+    
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+import nltk
+import os
+
+
+# Set the NLTK data path
+nltk.data.path.append(nltk_data_dir)
+# Define your preprocessing function (this is just a placeholder, replace with your actual preprocessing)
+ps = PorterStemmer()
+
+def transform_text(text):
+    # Convert text to lowercase
+    text = text.lower()
+    
+    # Remove punctuation
+    text = "".join([char for char in text if char not in string.punctuation])
+
+    # Tokenize the text
+    words = word_tokenize(text)
+
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+
+    # Stem the words
+    stemmed_words = [ps.stem(word) for word in words]
+
+    # Join the words back into a string
+    text = " ".join(stemmed_words)
+    
+    return text
 # Assuming you've already done preprocessing similar to spam classifier
 # and your sentiment analysis model is trained with the same preprocessing
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
@@ -60,38 +112,6 @@ with open("style.css") as f:
 st.title("Sentiment Analysis Classifier")
 
 input_text = st.text_area("Enter your text sentiment analysis") 
-
-import string
-import nltk
-nltk.download('punkt')
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem.porter import PorterStemmer
-
-# Define your preprocessing function (this is just a placeholder, replace with your actual preprocessing)
-ps = PorterStemmer()
-
-def transform_text(text):
-    # Convert text to lowercase
-    text = text.lower()
-    
-    # Remove punctuation
-    text = "".join([char for char in text if char not in string.punctuation])
-
-    # Tokenize the text
-    words = word_tokenize(text)
-
-    # Remove stopwords
-    stop_words = set(stopwords.words('english'))
-    words = [word for word in words if word not in stop_words]
-
-    # Stem the words
-    stemmed_words = [ps.stem(word) for word in words]
-
-    # Join the words back into a string
-    text = " ".join(stemmed_words)
-    
-    return text
 
 if st.button('Analyze Sentiment'):
     # 1. Preprocess the text (make sure this matches the preprocessing done during model training)
